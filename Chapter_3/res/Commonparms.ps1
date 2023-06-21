8aa427be38b550d2352c7366d0e4873a144967f2
Julius Angres <julius.angres@outlook.de>
2023-06-21 20:24:59 +0200

    Delete Chapter_3 directory

--- Chapter_3/res/Commonparms.ps1
+++
@@ -1,8 +0,0 @@
-# create structure for common parameter exercise
-sl $Env:userprofile\Downloads
-ni src -ItemType Directory
-ni dest -ItemType Directory
-ni src\a.txt 
-ni src\ro.txt
-sp src\ro.txt -Name IsReadOnly -Value $True
-sl src
